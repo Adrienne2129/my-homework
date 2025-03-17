f986d9807916b1ad5ab1d716fda73bffc79f2799
Alimzhan <muratulyalimzan@gmail.com>
2025-03-17 21:19:55 +0500

    Добавлены файлы шахматной доски и анимации

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-document.getElementById("username").addEventListener("input", function () {
-    this.value = this.value.replace(/\d/g, ""); 
-});
+const canvas = document.getElementById("chessboard");
+const ctx = canvas.getContext("2d");
+
+const size = 400;
+const cellSize = size / 8;
+canvas.width = canvas.height = size;
+
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+        ctx.fillStyle = (row + col) % 2 === 0 ? "#fff" : "#000";
+        ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
+    }
+}
